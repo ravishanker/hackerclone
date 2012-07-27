833c5f66116f0a604ad5b042b0e4143dc62062fa
Ravi Shanker <ravi.m.shanker@gmail.com>
2012-07-26 20:01:04 -0700

    HN Clone almost done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,21 @@
 class CommentsController < ApplicationController
 
+  before_filter :find_commentable 
 
   def index
-    @commentable = find_commentable
-    #audit @commentable
-    #@comments = @commentable.comments
+
+    @comments = []
+
+    if @commentable.respond_to?('each')
+      @commentable.each do |comment|
+        #audit comment
+        @comments << comment
+      end
+    else
+      @comments = @commentable.comments
+    end
+
+    @comments
 
     respond_to do |format|
       format.html # index.html.erb
@@ -14,8 +25,10 @@ class CommentsController < ApplicationController
   end
 
   def show
-    @comment = Comment.find(params[:id])
-    #@commentable = find_commentable
+    #@comment = Comment.find(params[:id])
+
+    @comment = @commentable.comments.find(params[:id])
+    
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @comment }
@@ -24,26 +37,28 @@ class CommentsController < ApplicationController
 
   def new
 
-    @comment = Comment.new
-    @commentable = find_commentable
-    #     @comments = @commentable.comments.build(params[:comment])
-
+    #@comment = Comment.new
+    @comment = @commentable.comments.build
+    
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @comments }
     end
   end
 
+  def edit
+    
+  end
+
   def create
 
-    @commentable = find_commentable
-    #audit @commentable.user_id
     @comment = @commentable.comments.build(params[:comment])
-    @comment.user_id = @commentable.user_id
+    @comment.user_id = current_user.id
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to news_comments_path , notice: 'Comment was successfully created.' }
+        format.html { redirect_to [@commentable, @comment] , notice: 'Comment was successfully created.' }
+        #format.html { redirect_to news_comments_path , notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render action: "new" }
@@ -52,26 +67,29 @@ class CommentsController < ApplicationController
     end
 
   end
+  
 
   private
 
-  def find_commentable
-    # params.each do |name, value|
-    #       if name =~ /(.+)_id$/
-    #         return $1.classify.constantize.find(value)
-    #       end
-    #     end
-    #     #raise ActiveRecord::NoRecord.new("Couldn\'t find it captain!")
-    #     nil
-        #audit params
-        if params[:comment_id]
-            return Comment.find(params[:comment_id])
-        elsif params[:news_id]
-            return News.find(params[:news_id])
-        else
-          return Comment.all
-        end
+  def find_commentable    
 
+    #audit params
+    if params[:comment_id]        
+        @commentable = Comment.find(params[:comment_id])
+
+    elsif params[:news_id]        
+        @commentable = News.find(params[:news_id])
+
+    elsif (params[:comment_id] && params[:id])
+        @commentable = Comment.find(params[:comment_id])
+
+    elsif (params[:news_id] && params[:id])
+        @commentable = News.find(params[:news_id])
+
+    else     
+      @commentable = Comment.all
+
+    end
   end
 
 
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,47 +1,97 @@
 class VotesController < ApplicationController
 
+  before_filter :find_votable 
+
   def index
-    @votable = find_votable
-    @votes = @votable.votes
+
+    @votes = []
+
+    if @votable.respond_to?('each')
+      @votable.each do |vote|
+        #audit comment
+        @votes << vote
+      end
+    else
+      @votes = @votable.votes
+    end
+
+    @votes
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @votes }
+      format.json { render json: @votable }
     end
 
+  end
+
+  def show
+    #@comment = Comment.find(params[:id])
+
+    @vote = @votable.votes.find(params[:id])
+    
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @vote }
+    end
   end
 
   def new
-    @votes = Vote.new
 
+    #@comment = Comment.new
+    @vote = @votable.votes.build
+    
     respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @votes }
+      format.html # new.html.erb
+      format.json { render json: @vote }
     end
+  end
 
+  def edit
+    
   end
 
   def create
-    @votable = find_votable
-    @vote = @votable.votes.build(params[:vote])
 
-    if @vote.save
-      format.html { redirect_to @vote, notice: 'Vote was successfully created.' }
-      format.json { render json: @vote, status: :created, location: @vote }
-    else
-      format.html { render action: "new" }
-      format.json { render json: @vote.errors, status: :unprocessable_entity }
+    @vote = @votable.votes.build(params[:vote])
+    @vote.vote = 1
+    @vote.user_id = current_user.id
+
+    respond_to do |format|
+      if @vote.save
+        format.js {  }
+        format.html { redirect_to [@votable, @vote] , notice: 'Vote was successfully created.' }
+        #format.html { redirect_to news_comments_path , notice: 'Comment was successfully created.' }
+        format.json { render json: @vote, status: :created, location: @vote }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @vote.errors, status: :unprocessable_entity }
+      end
     end
 
   end
+  
 
-  def find_votable
-    params.each do |name, value|
-      if name =~ /(.+)_id$/
-        return $1.classify.constantize.find(value)
-      end
+  private
+
+  def find_votable    
+
+    #audit params
+    if params[:comment_id]        
+        @votable = Comment.find(params[:comment_id])
+
+    elsif params[:news_id]        
+        @votable = News.find(params[:news_id])
+
+    elsif (params[:comment_id] && params[:id])
+        @commentable = Comment.find(params[:comment_id])
+
+    elsif (params[:news_id] && params[:id])
+        @commentable = News.find(params[:news_id])
+
+    else     
+      @votable = Vote.all
+
     end
-    nil
   end
 
 end
