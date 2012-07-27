64607ea674b373a84c8292cb479e8049fab24f60
Ravi Shanker <ravi.m.shanker@gmail.com>
2012-07-27 16:50:29 -0700

    deploying a version to heroku

--- app/models/news.rb
+++ app/models/news.rb
@@ -7,4 +7,13 @@ class News < ActiveRecord::Base
   has_many :votes, :as => :votable
   has_many :comments, :as => :commentable
 
+  before_create :add_http_to_url
+
+  def add_http_to_url
+  	u = URI.parse(self.url)
+  	unless u.scheme
+  		self.url = 'http://' + self.url
+  	end
+  end
+
 end
